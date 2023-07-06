7cb780373224e2da60a2d29f7361c624d63aedd8
jakubik307 <and.jj.an@gmail.com>
2023-07-06 12:01:13 +0200

    Backup

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,16 +7,22 @@ import javax.swing.*;
 public class Main {
 
     // PARAMETERS
-    public static final int MAX_KEYPOINTS = 10000;
+    public static final int MAX_KEYPOINTS = 10_000;
     public static final int CANNY_THRESHOLD1 = 130;
     public static final int CANNY_THRESHOLD2 = 320;
-    public static final double POINT_SPACING = 6;
+    public static final double POINT_SPACING = 8;
     public static final int MIN_CYCLE_LENGTH = 20;
     public static final int MAX_CYCLE_LENGTH = 40;
+
+    // MAX_KEYPOINTS set on 10_000 does not reduce generated points
+    // Canny parameters may change with different images
+
+    // Example p
+    // 8, 20, 40
+    // 11, 20, 35
+    // 15, 8, 30
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(GUI::new);
     }
 }
-
-// AgastFeatureDetector, BRISK, FastFeature,
